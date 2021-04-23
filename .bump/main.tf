6cb3e0a80e5e2f1f679c13ac50e27fb30f6def27
Alasdair Monk <alasdair.monk@gmail.com>
2021-04-23 13:19:25 +0000

    Add cors rules

--- .bump/main.tf
+++ .bump/main.tf
@@ -25,6 +25,14 @@ locals {
 resource "aws_s3_bucket" "bucket" {
   bucket = local.s3_bucket_name
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket_object" "changelog" {
